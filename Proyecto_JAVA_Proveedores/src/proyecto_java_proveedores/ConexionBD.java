fa2a24d7d355b06ce4c6f2380631209a741a7c37
Ángel David Franco Hernández <gelitofran@gmail.com>
2024-08-31 00:57:26 -0600

    Código para Conectar la Base de Datos
    Actualización para crear la conexión con la base de datos en JAVA. Nota: Es necesario cambiar el usuario, contraseña y el puerto en el que tu servidor de MySQL se ejecuta.

--- Proyecto_JAVA_Proveedores/src/proyecto_java_proveedores/ConexionBD.java
+++ Proyecto_JAVA_Proveedores/src/proyecto_java_proveedores/ConexionBD.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyecto_java_proveedores;
-
-/**
- *
- * @author Usuario
- */
+import java.sql.*; 
+import javax.swing.JOptionPane;
 public class ConexionBD {
+    /// Variables necesarias para la conexión con la Base de Datos.
+    /// Cambien el valor de las variables con sus datos del servidor MySQL.
+    String nombre_bd="proveedores"; 
+    String usuario="root";
+    String contraseña=""; 
+    String url="jdbc:mysql://localhost:3307/"+nombre_bd; 
+    String driver="com.mysql.cj.jdbc.Driver"; 
+    Connection Nueva_Conexion; 
+    
+    ///Constructor de la Clase ConexionBD.
+    public ConexionBD(){
+        try{
+            Class.forName(driver); 
+            Nueva_Conexion=DriverManager.getConnection(url, usuario, contraseña);
+            JOptionPane.showMessageDialog(null, "La Conexión a la Base de Datos fue exitosa.", "Notificación de Conexión", JOptionPane.INFORMATION_MESSAGE);
+        }catch(Exception e){
+          JOptionPane.showMessageDialog(null, "Error al Conectar la Base de Datos: "+e.toString(), "Notificación de Conexión", JOptionPane.ERROR_MESSAGE);   
+        }
+    }
     
 }
--- Proyecto_JAVA_Proveedores/src/proyecto_java_proveedores/Proyecto_JAVA_Proveedores.java
+++ Proyecto_JAVA_Proveedores/src/proyecto_java_proveedores/Proyecto_JAVA_Proveedores.java
@@ -1,20 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyecto_java_proveedores;
-
-/**
- *
- * @author Usuario
- */
 public class Proyecto_JAVA_Proveedores {
-
-    //Chinga tu madre javier
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ConexionBD Establecer_Conexion=new ConexionBD(); 
     }
-    
 }
